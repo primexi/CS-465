65b1d29d6948149ff2237af6d9289580c5665c31
Kris Foster <kfosterxi@gmail.com>
2024-07-28 22:54:05 -0400

    Module 4 completed baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -5,7 +5,7 @@ var trips = JSON.parse(fs.readFileSync('data/trips.json','utf8'));
 
 /* GET travel view */
 const travel = (req, res) => {
-    res.render('travel', {title: 'Travlr Getaways' , trips });
+    res.render('travel', {title: 'Travlr Getaways' , trips});
 };
 
 module.exports = {
